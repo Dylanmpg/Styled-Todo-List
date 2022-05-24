02bf17fa8e3af95a2c14452b8524349f34cc0af6
Dylanmpg <98571328+Dylanmpg@users.noreply.github.com>
2022-05-23 21:21:14 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'Go to gym',
+      isCompleted: false,
+    },
+    {
+      text: 'Drink post workout shake',
+      isCompleted: false,
+    },
+    {
+      text: 'Eat a high protein meal',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    <div className="app">
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
